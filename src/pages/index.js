aaa87dad060b2839670358ae1a5ee4dc314039e7
Jeremey <jeremey.duvall@automattic.com>
2017-12-04 17:25:07 -0700

    Index: Add GraphiQL query to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,34 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+class IndexPage extends React.Component {
+	render() {
+		const photos = this.props.data.allWordpressPost.edges;
 
-export default IndexPage
+		return (
+			<div>
+				{
+					photos.map( photo => {
+						return (
+							<p dangerouslySetInnerHTML={ { __html: photo.node.title } } />
+						)
+					} )
+				}
+			</div>
+		)
+	}
+}
+
+export default IndexPage;
+
+export const pageQuery = graphql`
+	query IndexQuery {
+		allWordpressPost( sort: { fields: [ date ], order: DESC } ) {
+			edges {
+				node {
+					title
+				}
+			}
+		}
+	}
+`
